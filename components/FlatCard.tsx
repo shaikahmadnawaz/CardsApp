000db6a110c2f393a6d93dd5c1affbb34bd6a82a
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-25 17:42:32 +0530

    implemented cards using flexbox

--- components/FlatCard.tsx
+++ components/FlatCard.tsx
@@ -9,6 +9,15 @@ export default function FlatCard() {
         <View style={[styles.card, styles.cardOne]}>
           <Text>Red</Text>
         </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardFour]}>
+          <Text>Orange</Text>
+        </View>
       </View>
     </View>
   );
@@ -20,15 +29,30 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 10,
   },
-  container: {},
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 10,
+  },
   card: {
     flex: 1,
     justifyContent: 'center',
-    alignContent: 'center',
+    alignItems: 'center',
     width: 100,
     height: 100,
     borderRadius: 4,
     margin: 8,
   },
-  cardOne: {},
+  cardOne: {
+    backgroundColor: 'red',
+  },
+  cardTwo: {
+    backgroundColor: 'green',
+  },
+  cardThree: {
+    backgroundColor: 'blue',
+  },
+  cardFour: {
+    backgroundColor: 'orange',
+  },
 });
